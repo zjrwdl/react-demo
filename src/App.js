6340cb7a43cdba563d6731e0800737c2213ae685
rockypzhang <rockypzhang@tencent.com>
2023-03-09 00:01:19 +0800

    optimize code catagorize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/nav.css';
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home"
@@ -7,14 +8,10 @@ import About from "./pages/About"
 function App() {
     return (
         <Router>
-            <div className="Demo">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
+            <div>
+                <ul className="nav">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
                 </ul>
 
                 <Routes>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function About() {
+    const numbers = [
+        "1、how to use Router",
+        "2、how to use net net component",
+        "3、how to display image/video",
+        "3、how to display list view",
+        "4、how to connect wallet by ether.js or web3.js",
+    ];
+    const listItems = numbers.map((number) =>
+        <li>{number}</li>
+    );
     return (
         <div>
-            <h2>About</h2>
+            <h3>This is a react learning case :</h3>
+            <ul>{listItems}</ul>
         </div>
     );
 }
