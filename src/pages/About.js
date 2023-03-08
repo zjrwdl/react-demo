0cb33d084b3a34f4b7e8d5a6ace818667cd6c677
rockypzhang <rockypzhang@tencent.com>
2023-03-09 00:23:53 +0800

    optimize code catagorize

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,10 @@ function About() {
         "2、how to use net component",
         "3、how to display image/video",
         "4、how to display list view",
-        "5、how to connect wallet by ether.js or web3.js",
+        "5、how to persist data in local storage",
+        "6、how to use provider/redux",
+        "7、how to modularize",
+        "8、how to connect wallet by ether.js or web3.js",
     ];
     const listItems = numbers.map((number) =>
         <li>{number}</li>
