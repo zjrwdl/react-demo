df035b840424c9d9bb9fdd46c61844f90f3db6da
rockypzhang <rockypzhang@tencent.com>
2023-03-09 00:06:41 +0800

    optimize code catagorize

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
     const numbers = [
         "1、how to use Router",
-        "2、how to use net net component",
+        "2、how to use net component",
         "3、how to display image/video",
         "4、how to display list view",
         "5、how to connect wallet by ether.js or web3.js",
